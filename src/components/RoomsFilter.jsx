27ddcfb5622be0b036a03d7654785ea95d198a58
omarmosaad27 <omarmosaada27@gmail.com>
2021-04-20 19:36:35 +0200

    u

--- src/components/RoomsFilter.jsx
+++ src/components/RoomsFilter.jsx
@@ -35,12 +35,12 @@ const RoomsFilter = () => {
             <Title title="search rooms" /> 
             <form className="filter-form">
                 {/* select type */}
-                <div className="form-group">
+                <section className="form-group">
                     <label htmlFor="type">room type</label>
                     <select name="type" id="type" value={type} className="form-control" onChange={handleChange}>
                         {types}
                     </select>
-                </div>
+                </section>
                 {/* end select type */}
             </form>
         </section>
